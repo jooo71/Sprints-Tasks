a13861019c457d8deaa6d6da37e1a932400c9950
youseif essam <93451542+jooo71@users.noreply.github.com>
2024-08-24 14:29:10 +0300

    Delete Moving Car directory

--- Moving Car/led_version1_sprint/led_version1_sprint/Debug/MCAL/Interrupt/interrupt.d
+++
@@ -1,9 +0,0 @@
-MCAL/Interrupt/interrupt.d MCAL/Interrupt/interrupt.o: \
- ../MCAL/Interrupt/interrupt.c ../MCAL/Interrupt/interrupt.h \
- ../APPLICATION/../util/registers.h ../APPLICATION/../util/typdef.h
-
-../MCAL/Interrupt/interrupt.h:
-
-../APPLICATION/../util/registers.h:
-
-../APPLICATION/../util/typdef.h:
--- Moving Car/led_version1_sprint/led_version1_sprint/Debug/MCAL/dio/dio.d
+++
@@ -1,8 +0,0 @@
-MCAL/dio/dio.d MCAL/dio/dio.o: ../MCAL/dio/dio.c ../MCAL/dio/dio.h \
- ../APPLICATION/../util/registers.h ../APPLICATION/../util/typdef.h
-
-../MCAL/dio/dio.h:
-
-../APPLICATION/../util/registers.h:
-
-../APPLICATION/../util/typdef.h:
